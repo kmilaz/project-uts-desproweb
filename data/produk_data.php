2a0717deb57a20c73b49b795dfadee9cd0f90c6c
Kamila Zahwa <kamilazhwa@gmail.com>
2025-10-15 09:39:04 +0700

    add: data product
    Ini adalah simulasi database

---
+++ data/produk_data.php
@@ -0,0 +1,85 @@
+<?php
+$products = [
+    [
+        "id" => 1,
+        "name" => "Pastel Beads",
+        "price" => 12500,
+        "image" => "assets/img/pastel-beads.png",
+        "category" => "Beads",
+        "tags" => "Pastel, Beads",
+        "description" => "Lembut, ceria, dan lebih gemas! Koleksi manik-manik pastel hadir untuk kamu para craft-enthusiast."
+    ],
+    [
+        "id" => 2,
+        "name" => "Lily",
+        "price" => 15000,
+        "image" => "assets/img/lily.png",
+        "category" => "Beads",
+        "tags" => "Flower, Beads",
+        "description" => "Manik-manik berbentuk bunga lily yang lucu dan menggemaskan."
+    ],
+    [
+        "id" => 3,
+        "name" => "Pearl Beads",
+        "price" => 12500,
+        "image" => "assets/img/pearls.png",
+        "category" => "Beads",
+        "tags" => "Pearl, Beads",
+        "description" => "Manik-manik mutiara dengan warna-warni pastel yang cantik."
+    ],
+    [
+        "id" => 4,
+        "name" => "Flower Beads",
+        "price" => 17000,
+        "image" => "assets/img/flower-beads.png",
+        "category" => "Charms",
+        "tags" => "Flower, Charm",
+        "description" => "Charm berbentuk bunga untuk melengkapi kreasi Anda."
+    ],
+    [
+        "id" => 5,
+        "name" => "Scallop Beads",
+        "price" => 20000,
+        "image" => "assets/img/scallop-beads.png",
+        "category" => "Beads",
+        "tags" => "Scallop, Sea",
+        "description" => "Manik-manik berbentuk kerang yang indah."
+    ],
+    [
+        "id" => 6,
+        "name" => "Pink Galaxy Beads",
+        "price" => 17000,
+        "image" => "assets/img/pink-galaxy.png",
+        "category" => "Beads",
+        "tags" => "Pink, Galaxy",
+        "description" => "Manik-manik bulat dengan sentuhan galaksi berwarna pink."
+    ],
+    [
+        "id" => 7,
+        "name" => "Respira",
+        "price" => 25000,
+        "image" => "assets/img/respira.png",
+        "category" => "Pendant",
+        "tags" => "Fruits, Pendant",
+        "description" => "Pendant lucu berbentuk aneka buah-buahan."
+    ],
+    [
+        "id" => 8,
+        "name" => "Bee Beads",
+        "price" => 21500,
+        "image" => "assets/img/bee-beads.png",
+        "category" => "Beads",
+        "tags" => "Bee, Animal",
+        "description" => "Manik-manik berbentuk lebah yang sangat unik."
+    ],
+    [
+        "id" => 9,
+        "name" => "Berry Beads",
+        "price" => 20500,
+        "image" => "assets/img/berry-beads.png",
+        "category" => "Charms",
+        "tags" => "Berry, Fruits",
+        "description" => "Charm berbentuk buah berry yang segar."
+    ]
+];
+?>
