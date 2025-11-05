fc8051dad1ffc026c0aeb8c75b3222e63981e04e
Kamila Zahwa <kamilazhwa@gmail.com>
2025-11-05 09:25:29 +0700

    add icon

--- partials/header.php
+++ partials/header.php
@@ -14,6 +14,7 @@ include 'database/migrasi.php';
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
 </head>
 <body>
     <header class="header">
