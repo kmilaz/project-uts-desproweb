49cf57daebac37d4866feea44f6d9c980d03e3cf
Kamila Zahwa <kamilazhwa@gmail.com>
2025-11-13 21:50:37 +0700

    refactor database connection to use singleton function

--- database/koneksi.php
+++ database/koneksi.php
@@ -1,23 +1,37 @@
 <?php
-$host = 'localhost';
-$port = '5432';
-$user = 'postgres';
-$pass = '12345678';
-$database = 'dainty_doodles';
 
-$connection = pg_connect("host=$host port=$port dbname=$database user=$user password=$pass");
-if (!$connection) {
-    $errorMessage = pg_last_error();
-    die("connection failed, error: $errorMessage");
+function get_connection() : \PgSql\Connection {
+    $host = 'localhost';
+    $port = '5432';
+    $user = 'postgres';
+    $pass = '12345678';
+    $database = 'dainty_doodles';
+
+    static $connection;
+
+    if ($connection instanceof \PgSql\Connection) {
+        return $connection;
+    }
+
+    $connectionString = "host=$host port=$port dbname=$database user=$user password=$pass";
+
+    $connection = pg_connect($connectionString);
+
+    if (!$connection) {
+        $errorMessage = pg_last_error();
+        die("connection failed, error: $errorMessage");
+    }
+
+    if (pg_set_client_encoding($connection, 'UTF8') == 1) {
+        $errorMessage = pg_last_error();
+        die("fail to set enconding, error: $errorMessage");
+    }
+
+    return $connection;
 }
 
-if (pg_set_client_encoding($connection, 'UTF8') == 1) {
-    $errorMessage = pg_last_error();
-    die("fail to set enconding, error: $errorMessage");
-}
-
-function query($sql, $connection) {
-    $result = pg_query($connection, $sql);
+function query($sql) {
+    $result = pg_query(get_connection(), $sql);
 
     if (!$result) {
         $errorMessage = pg_last_error();
